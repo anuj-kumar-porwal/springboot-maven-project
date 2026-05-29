4e443e97164067a883fcf63b23b3561733fe25e8
anuj kumar <anujkumar13390@gmail.com>
2026-05-29 16:02:17 +0530

    message 5

--- src/main/java/com/ak/DemoProjectApplication.java
+++ src/main/java/com/ak/DemoProjectApplication.java
@@ -2,13 +2,19 @@ package com.ak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoProjectApplication {
+public class DemoProjectApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoProjectApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoProjectApplication.class, args);
-
 		System.out.println("hello world ....");
 
 	}
